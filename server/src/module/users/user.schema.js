e18c3780a981b0a8b8ed8fc384c2358c4def7503
naffiziqbal <f.nafiziqbal@gmail.com>
2023-11-27 22:59:48 +0600

    Password Hasing

--- server/src/module/users/user.schema.js
+++ server/src/module/users/user.schema.js
@@ -1,4 +1,5 @@
 const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   name: String,
@@ -6,7 +7,17 @@ const UserSchema = new mongoose.Schema({
   password: String,
 });
 
+UserSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
 
- const User = mongoose.model('User', UserSchema)
+const User = mongoose.model("User", UserSchema);
 
- module.exports = User
+module.exports = User;
