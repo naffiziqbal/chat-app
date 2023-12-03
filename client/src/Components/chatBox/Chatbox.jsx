946dd0db7ea08c4e5d519255494d47ecafa90801
naffiziqbal <f.nafiziqbal@gmail.com>
2023-12-03 11:17:06 +0600

    Input Emoji

--- client/src/Components/chatBox/Chatbox.jsx
+++ client/src/Components/chatBox/Chatbox.jsx
@@ -1,18 +1,17 @@
 import { useEffect, useState } from "react";
-import { HiEmojiHappy, HiOutlineDotsVertical, HiPhone, HiVideoCamera } from "react-icons/hi";
+import { HiOutlineDotsVertical, HiPhone, HiVideoCamera } from "react-icons/hi";
 import { useParams } from "react-router-dom";
-import {  LuSticker } from "react-icons/lu";
+import { LuSticker } from "react-icons/lu";
 import { AiOutlineGif } from "react-icons/ai";
 import { FaArrowLeft, FaImage } from "react-icons/fa6";
-import { IoSend} from "react-icons/io5";
-
-
+import { IoSend } from "react-icons/io5";
+import InputEmoji from "react-input-emoji"
 
 const Chatbox = () => {
     const { id } = useParams()
     const [user, setUser] = useState([])
-    const [inputValue, setInputValue] = useState('');
     const [isTyping, setIsTyping] = useState(false);
+    const [text, setText] = useState('')
 
     useEffect(() => {
         fetch(`/chat.json`)
@@ -22,23 +21,30 @@ const Chatbox = () => {
 
     const singleUser = user.find(data => data?.id === id)
 
-    const handleEnter = (event) => {
-        if (event.key === 'Enter' && !event.shiftKey) {
-            event.preventDefault();
-            // Add your custom logic here, for example, adding a new line break or submitting the form.
-            alert('Enter key pressed without Shift!');
+    const handleOnChange = (e) => {
+        if (e.key === "Enter") {
+            console.log("13")
         }
+        if (e.length > 0) {
+            setIsTyping(true)
+        }
+        const text = e;
+        setText(text);
     };
-    const handleInputChange = (e) => {
-        const textarea = e.target;
-        textarea.style.height = 'auto';
-        textarea.style.height = `${textarea.scrollHeight}px`;
-        setInputValue(textarea.value);
-        const inputText = e.target.value;
-        setInputValue(inputText);
-        // Check if the user is typing
-        setIsTyping(inputText.length > 0);
-    };
+
+    const handleFormSubmit = () => {
+        // Post Logic Gose Here 
+
+    }
+    const handleEnter = (e) => {
+        if (e.key === "Enter") {
+            setText('')
+            return handleFormSubmit()
+        }
+
+    }
+
+
     return (
         <div className={`lg:w-2/3 w-full relative`}>
             {
@@ -71,8 +77,8 @@ const Chatbox = () => {
                     </div>
                     {/*  Form  */}
                     <footer className="">
-                        <form className=" bg-slate-200 flex flex-row absolute bottom-3 right-0 w-full items-center justify-between">
-                            <span className=" mx-1 cursor-pointer "
+                        <form onSubmit={handleFormSubmit} className=" flex flex-row absolute bottom-3 right-0 w-full items-center justify-between">
+                            <span className=" cursor-pointer "
                                 onClick={() => setIsTyping(!isTyping)}
                             >
                                 {
@@ -96,24 +102,20 @@ const Chatbox = () => {
                             </div>
 
                             <div className="w-full flex  items-center justify-end">
-                                <input
-                                    className="max-h-24 outline-none duration-300 rounded-3xl items-center justify-center py-3 px-5"
-                                    style={{ width: isTyping ? '95%' : '50%', resize: 'none', overflow: 'hidden' }}
-                                    value={inputValue}
-                                    onChange={handleInputChange}
-                                    onKeyDown={handleEnter}
-                                    placeholder="Aa"
-                                ></input>
-                                <span className="">{<HiEmojiHappy style={{
-                                    height: "1.2rem",
-                                    width: '1.2rem',
-                                    margin: '0 1rem'
-                                }} />}</span>
+                                <span className="w-full outline-none duration-300 rounded-3xl items-center justify-center overflow-hidden">
+                                    <InputEmoji className="max-w-sm"
+                                        value={text}
+                                        onKeyDown={handleEnter}
+                                        onChange={handleOnChange}
+                                        placeholder="Type a message"
+                                    />
+                                </span>
+                                <button type="submit"
+                                    className="bg-blue-500 text-accent p-2 rounded max-h-10 items-center mx-3"
+                                >{<IoSend />}</button>
                             </div>
 
-                            <button type="submit"
-                                className="bg-blue-500 text-white p-2 rounded max-h-10 items-center mx-3"
-                            >{<IoSend />}</button>
+
                         </form>
                     </footer>
                 </>
